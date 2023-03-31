c6b63c99e43aa3b7bcf64671736dd8e4a7be7ae1
s11verz <72031018+s11verz@users.noreply.github.com>
2023-03-31 14:02:40 +0900

    삭제

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import Todo from "../models/todo";
 import classes from "./TodoItem.module.css";
 
-const TodoItem: React.FC<{ item: Todo }> = (props) => {
+const TodoItem: React.FC<{ item: Todo; onRemoveTodo: () => void }> = (
+  props
+) => {
   return (
-    <li className={classes.item} key={props.item.id}>
+    <li
+      className={classes.item}
+      key={props.item.id}
+      onClick={props.onRemoveTodo}
+    >
       {props.item.text}
     </li>
   );
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -3,11 +3,16 @@ import Todo from "../models/todo";
 import TodoItem from "./TodoItem";
 import classes from "./Todos.module.css";
 
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (
+  props
+) => {
   return (
     <ul className={classes.todos}>
       {props.items.map((item) => (
-        <TodoItem item={item} />
+        <TodoItem
+          item={item}
+          onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}
+        />
       ))}
     </ul>
   );
